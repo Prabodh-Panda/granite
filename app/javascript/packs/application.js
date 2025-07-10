72855b10db2498e6dc411761fbc13e2fb0f49c8a
Prabodh Kumar Panda <connect@prabodh.co.in>
2025-07-10 17:27:51 +0530

    Created Toastr component and added Axios interceptors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,16 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails";
-import "../controllers";
 import "../stylesheets/application.scss";
 import ReactRailsUJS from "react_ujs";
 import App from "../src/App";
 
-import { setAuthHeaders } from "apis/axios";
-import { initializeLogger } from "../src/common/logger";
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
 
+registerIntercepts();
 initializeLogger();
 setAuthHeaders();
 
 const componentsContext = { App };
-ReactRailsUJS.getConstructor = (name) => {
+ReactRailsUJS.getConstructor = name => {
   return componentsContext[name];
 };
